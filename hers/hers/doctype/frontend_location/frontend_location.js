ed6eb65c22f4c15ee8a0203cb3f81cdc1223d459
SindhuDhanushkodi <sindhu@yuvabe.com>
2023-12-08 17:07:35 +0530

    first commit

---
+++ hers/hers/doctype/frontend_location/frontend_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Sindhu and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Frontend Location', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ hers/hers/doctype/warehouse_location/warehouse_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Sindhu and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Warehouse Location', {
+	// refresh: function(frm) {
+
+	// }
+});
